7c339c63f44215e11efeb48e598cde8f5569b444
mariam <mmarzouk3@gatech.edu>
2020-01-29 14:24:32 -0500

    print sim title

--- mainWorld.py
+++ mainWorld.py
@@ -4,6 +4,26 @@ import Event
 
 from queue import PriorityQueue
 
+#a startup method with some kind of simulation
+#initialization behavior. for now just prints
+#the name of the simulator in ascii
+def startup():
+	print( 
+		   '___________              _____  _____. __                  \n'
+		   '\__    ___/___________ _/ ____\/ ____\|__| ____            \n' 
+		   '  |    |  \_  __ \__ \.\   __\.\   __\|  |/ ___\           \n' 
+		   '  |    |   |  | \// __ \|  |   |  |   |  \  \___           \n' 
+		   '  |____|   |__|  (____  /__|   |__|   |__|\___  >          \n' 
+		   '                      \/                     \/           \n' 
+		   '  _________.__              .__           __                \n'
+		   ' /   _____/|__| _____  __ __|  | _____  _/  |_  ___________  \n'
+		   ' \_____  \ |  |/     \|  |  \  | \__  \.\   __\/  _ \_  __ \ \n'
+		   ' /        \|  |  Y Y  \  |  /  |__/ __ \|  |  (  <_> )  | \/ \n'
+		   '/_______  /|__|__|_|  /____/|____(____  /__|   \____/|__|    \n'
+		   '        \/          \/                \/                    \n'
+		 )
+
+startup()
 fel = PriorityQueue() #the future event list
 currTime = 0
 
